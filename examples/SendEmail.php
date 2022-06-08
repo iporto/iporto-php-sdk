56a7bdf27466cd6d061ae1f908a2ad93ff1c73a4
Otavio Nogueira <otavio@iporto.com>
2022-06-08 17:03:21 -0300

    Atualizado exemplo de uso.

--- examples/SendEmail.php
+++ examples/SendEmail.php
@@ -8,7 +8,7 @@ use iPORTO\Mail\MailClient;
  * Token gerado em: https://app.iporto.com.br/panel/api
  */
 $auth_bearer =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImRiNTkyMWU4YTRiNWY5MGFmMmQ3NWY5NDg2YzJlNTNxWDg2NWYzYmQyNjgyYTg1NTFkYjJjMDliZDRjNcFlOGYyZDVmNTc2MDY5YWFhYjRmIn0.eyJhdWQiOiIxIiwianRpIjoiZGI1OTIxZThhNGI1ZjkwYWYyZDc1Zjk0ODZjMmU1MzEwODY1ZjNiSDI2ODJhODU1MWRiMmMwOWJkNGM2YWU4ZjJkNWY1NzYwNjlhYWFiNGYiLCJpYXQiOjE2MDI3Njc5MzMsIm5iZiI6MTYwMjc2NzkzMywiZXhwIjoxNjM0MzAzOTMzLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.gbz-YHDd0RS8CD01xtD5FN-ovqai0dxK_TrWanJmwsQDSdvVA99VV_-_fQ4Lm-Wa3m9UFg6pc8-R5UYLADpzXC1ppUxwH3DexssC33kaOolalmOo8CQLQJFCDTvuzIxBH4qo_dBsVIsQ94EwcbD6r2GsIBuHZjW5DjCp-eR1UJGacS27MAfnnU-7pBUrNM2vei1Br1-xzSU4caDQ_eNUvV0XhpWxDQ69sPxGsvLmMoD7nGibkBikIppO7L927ruoIMZ13kF7gMrWLIv8f5ecvOuMC4He159FoYfBXimAJ0NJhDz2c8g9GQKOAE5OYGwndQZk0kh4KbL2Rf6gxEeQcco20aLjjwPX6QlKO6BuWyedbyBLEY_vC71P4VHfy7TKvJxTYUKH9K-Qs8M5pWdQLQpiN676BXCNTad5-bLxy88YdM_SzxDeskh9XWjVPgeeiDfjf3u-NoEvwr_rH1s_zMvdgJtl0lsmiAtkIDRForHaH88v1InQJQi0gLb2Ypcgra1UtrJb4NbbCqc2XIKldcnvjQxbm9-VWsOjLrfYYpALa0tS1KVWtUSY4AWt44q_wXo8xZ97YpUV9i25dvwLcwPcy_J7rzSnQxthBttzFf4J5ywl-w811dxJXo7XGxjKDdeF0Z_oCB0WzWzIvxukxBiQMGqR9IfKhYeDGGHDX2A';
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYzUyZDk3ZjdmZjc1M2IxODkzOTZmMDVjNmQwYmJhN2ZkMDhhZjhiNmJjZDdlNzJiYzVjNjdhODg1MWFkMzI0MGUxMzFjMzg5NDMzNzU4ZGYiLCJpYXQiOjE2NDM0MDU1NjYsIm5iZiI6MTY0MzQwNTU2NiwiZXhwIjoxODAxMTcxOTY2LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.g1hfGADOGDErgyfYw6WHoHDBSC4x_td51-XNIyygm0rB_32ZEqG29AgoA9wp2VIgf3nIvjBp3NMyqYQ194znKj3Bp610Vj0clTEIw_f-hegwFFRvv6QiQnIIAiRNuSy9Mj94nokdVeud-_Uvld_IZ7dJak0nuR2Rk6dTHDPC2lOo__72nAqZwSQtvFg0t3UEl1rMowR4RpbLgGaUhrrpmnNWGGaxdNxAwSF8TIprUCW3QyO3xvQk_BOAnm9S2iMTR2DKBP8aZuuD-aiW5Rn0BOQu5vOEEq1uS8Q0AzItHSaqhWrnUewWK2HKv_kw-sHSUtO1gKuu2dp6bLpkfXoXAlT3FkMGXxAQRIH_jW_QnISxoDsy_1GG_f75pk4m1RU9Lr8zhYg_pmAajNQOjtKkPb23ARO1JxntP1fLRw4QsRBUBTSOh-Kk8s1i8s1d5y-K9e9rTqwk4SPJvPdC7KvoiEoeRkcG29vHz4HRQUeOiPJnf388KaWbjgxgEfXrS1Q2JQKW3lcgFYtOlTyu16Yk6HYRIp63A35evHkJWye2aGvMoyNbPqxfxanY9a1DHnOiMD8h8SsqWyrVMVhwiIMlBZmN-lpb7cIBzDVjtc2YMYQsyJhdHi61T13zlxCHSunR9ZpmDWTKrELL9rc7s9cR3uhU0FPUzFferZp3sI6BZKs';
 // -----------------------------------------------------
 
 /**
@@ -28,7 +28,7 @@ $objMail->setEmailHtmlBody(
 $objMail->setEmailSubject('iPORTO: ' . date('H:i:s'));
 $objMail->setEmailHeaders(['X-API-Company: iPORTO', 'X-API-SDK: PHP']);
 $objMail->setEmailHeadersTags(['iPORTO', 'API', 'External']);
-$objMail->setTrackingSettings(true, true);
+$objMail->setTrackingSettings(true, true, 'track-s1');
 $setMail = $objMail->send();
 
 dd($setMail);
